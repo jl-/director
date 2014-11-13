1cbaace49ff53f8d7359202d0d7845b3c65ce6f9
jl- <i.dragonxx@gmail.com>
2014-11-13 15:12:30 +0800

    throttle

--- director.js
+++ director.js
@@ -1,70 +1,99 @@
 ;
 define(['jquery'], function($) {
     var director = {};
-    var actors = [];
-    var staging = true;
 
 
-    director.assign = function($actor, onAppear, onActing, onDisappear) {
+    director.actors = [];
+
+    director.options = {
+        delay: 10,
+        flush_after: 30,
+        actors_appear_once: true,
+        actors_disappear_once: true
+    };
+
+    function throttle(fn, delay, mustRunAfter) {
+        var timer = null,
+            t_pre = +new Date();
+        return function() {
+            var context = this,
+                args = arguments,
+                t_curr = +new Date();
+            clearTimeout(timer);
+            if (t_curr - t_pre >= mustRunAfter) {
+                fn.apply(context, args);
+                t_pre = t_curr;
+            } else {
+                timer = setTimeout(function() {
+                    fn.apply(context, args);
+                }, delay);
+            }
+        };
+    }
+
+    director.config = function(conf) {
+        $.extend(director.options, conf);
+    };
+
+    director.assign = function($actor, onAppear, onActing, onDisappear, options) {
         if (typeof onAppear === 'function' || typeof onActing === 'function' || typeof onDisappear === 'function') {
             $actor.each(function() {
                 var $self = $(this);
                 $self.onAppear = onAppear;
                 $self.onActing = onActing;
                 $self.onDisappear = onDisappear;
-                actors.push($self);
-                staging = true;
+                $self.options = $.extend({}, options);
+                director.actors.push($self);
             });
         }
         return this;
     };
+
 
     director.direct = function() {
         var $window = $(window),
             data = {},
             actorIndex,
             $actor = null;
-        $window.scroll(function(){
-            staging = true;
-        });
-
         function action() {
-            if (staging) {
-                data.winScrollTop = $window.scrollTop();
-                data.winHeight = $window.height();
-                data.winWidth = $window.width();
-                for (actorIndex = actors.length - 1; actorIndex >= 0; actorIndex--) {
-                    $actor = actors[actorIndex];
-                    if (!$actor.onAppear && !$actor.onActing && !$actor.onDisappear) {
-                        actors.splice(actorIndex, 1);
-                        continue;
-                    }
-                    data.actorOffsetTop = $actor.offset().top;
-                    data.actorHeight = $actor.outerHeight();
+            data.winHeight = $window.height();
+            data.winWidth = $window.width();
+            for (actorIndex = director.actors.length - 1; actorIndex >= 0; actorIndex--) {
+                $actor = director.actors[actorIndex];
+                if (!$actor.onAppear && !$actor.onActing && !$actor.onDisappear) {
+                    director.actors.splice(actorIndex, 1);
+                    continue;
+                }
 
-                    if (data.winScrollTop + data.winHeight > data.actorOffsetTop && data.actorOffsetTop + data.actorHeight > data.winScrollTop) {
-                        $actor.appearing = true;
-                        if ($actor.onAppear) {
-                            $actor.onAppear.call(this, $actor);
+                data.winScrollTop = $window.scrollTop();
+                data.actorOffsetTop = $actor.offset().top;
+                data.actorHeight = $actor.outerHeight();
+
+                if (data.winScrollTop + data.winHeight > data.actorOffsetTop && data.actorOffsetTop + data.actorHeight > data.winScrollTop) {
+                    if (!$actor.appearing && $actor.onAppear) {
+                        $actor.onAppear.call(this, $actor);
+                        if($actor.options.appear_once === true || ($actor.options.appear_once !== false && director.options.actors_appear_once)){
                             delete $actor.onAppear;
                         }
-                        if($actor.onActing){
-                            $actor.onActing.call(this,$actor,data);
-                        }
-                    } else if ($actor.appearing) {
-                        $actor.appearing = false;
-                        if($actor.onDisappear)
+                    }
+                    if ($actor.onActing) {
+                        $actor.onActing.call(this, $actor, data);
+                    }
+                    $actor.appearing = true;
+                } else if ($actor.appearing) {
+                    $actor.appearing = false;
+                    if ($actor.onDisappear){
                         $actor.onDisappear.call(this, $actor);
+                    }
+                    if($actor.options.disappear_once === true || ($actor.options.disappear_once !== false && director.options.actors_disappear_once)){
                         delete $actor.onDisappear;
                     }
                 }
-                staging = false;
-            }
-            if (actors.length > 0) {
-                window.setTimeout(action, 0);
             }
         }
-        window.setTimeout(action, 0);
+        action();
+        $window.scroll(throttle(action,director.options.delay,director.options.flush_after));
+
         delete director.direct;
     };
 
